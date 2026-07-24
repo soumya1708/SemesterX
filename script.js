2d92dab511ac45753702f1801538e1ad80b2084d
Soumya1708 <soumya.mondal1708@gmail.com>
2026-07-24 19:34:05 +0530

    Update BACKEND_URL to production server

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
                     CONFIGURATION
 ==========================================================*/
 
-const BACKEND_URL = "http://localhost:8081";
+const BACKEND_URL = "https://semesterx-production.up.railway.app";
 
 const CONFIG = {
 
