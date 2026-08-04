0fbbeda73b1cdc5f9d7d9212f4f66607e34783c7
Soumya1708 <soumya.mondal1708@gmail.com>
2026-08-04 15:14:23 +0530

    Refactor semester 2 subjects based on department

--- script.js
+++ script.js
@@ -1403,19 +1403,29 @@ function renderSubjectResourceCards() {
             }
 
         }
-
-        // ================= SEMESTER 2 =================
-        else if (appState.currentSem === "2") {
-
-            subjects = [
-                "Chemistry I (BS-CH201)",
-                "English (HM-HU201)",
-                "Programming for Problem Solving (BS-CS201)",
-                "Mathematics IIA (BS-M201)"
-            ];
-
+          // ================= SEMESTER 2 ================
+      else if (appState.currentSem === "2") {
+        // ECE & ECS
+        if (
+          appState.currentDept === "ECE" ||
+          appState.currentDept === "ECS"
+        ) {
+          subjects = [
+            "Physics I (BS-PH101)",
+            "Mathematics IIB (BS-M202)",
+            "English (HM-HU201)",
+            "Programming for Problem Solving (ES-CS201)"
+          ];
         }
-
+        // IT, ME and other departments
+        else {
+          subjects = [
+            "Chemistry I (BS-CH201)",
+            "English (HM-HU201)",
+            "Programming for Problem Solving (BS-CS201)",
+            "Mathematics IIA (BS-M201)"
+          ];
+        }
     }
 
     if (subjects.length === 0) {
